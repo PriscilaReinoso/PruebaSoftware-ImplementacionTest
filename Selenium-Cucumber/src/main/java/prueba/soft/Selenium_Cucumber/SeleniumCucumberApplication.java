299029234d14d23278e853317c9b60947ddc3503
PriscilaReinoso <reinosopriscila.pr@gmail.com>
2025-11-04 19:47:11 -0300

    prueba login exitoso con selenium

---
+++ Selenium-Cucumber/src/main/java/prueba/soft/Selenium_Cucumber/SeleniumCucumberApplication.java
@@ -0,0 +1,13 @@
+package prueba.soft.Selenium_Cucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumCucumberApplication.class, args);
+	}
+
+}
---
+++ Selenium-Cucumber/src/test/java/prueba/soft/Selenium_Cucumber/SeleniumCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.soft.Selenium_Cucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
